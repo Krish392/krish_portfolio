f6db8b2f4c3fbcbd94d3e2b735c2380b7dcb48f8
Krishma sri <sriduraivr@gmail.com>
2026-08-28 15:17:30 +0530

    Add files via upload

---
+++ My Portfolio/script.js
@@ -0,0 +1,41 @@
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+sections.forEach((section) => {
+    section.classList.add("hidden");
+    observer.observe(section);
+});
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
